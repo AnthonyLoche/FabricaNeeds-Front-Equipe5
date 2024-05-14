48d3d312fddb366e090b1f275e6888470e0727ea
AnthonyLoche <anthonylocheifc@gmail.com>
2024-05-14 15:50:58 -0300

    FIX vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import Pages from 'vite-plugin-pages';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Pages({
-      pagesDir: 'src/views', // Defina o diretório onde estão seus componentes de página Vue
-      extensions: ['vue'], // Defina as extensões de arquivo para seus componentes Vue
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
