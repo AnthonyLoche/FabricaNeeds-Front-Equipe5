9322e908d598151f0fbf390dcd4363d66b0e6a9f
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-05-15 15:31:28 -0300

    barra de pesquisa funcional em estoque

--- src/components/Estoque/DemandasView.vue
+++ src/components/Estoque/DemandasView.vue
@@ -4,14 +4,15 @@ const demandas = ref([])
 const estoque = ref([])
 import { carregar } from '@/api/api';
 import { adicionar } from '@/api/api';
+carregar('demandas/', demandas)
+carregar('estoque/', estoque)
 
 const demanda = reactive({
     quantidade: 0,
     produto: '',
 })
 
-carregar('demandas/', demandas)
-carregar('estoque/', estoque)
+
 const modalAddIten = ref(null)
 </script>
 
@@ -19,45 +20,51 @@ const modalAddIten = ref(null)
     <main>
         <section>
             <h2>Demandas Atuais:</h2>
+            
             <div class="rolagemItens">
-    <div class="item" v-for="demanda in demandas" :key="demanda.id">
-    <div class="headerItem"><p>ID:</p>
-    <p>PRODUTO:</p>
-    <p>QUANTIDADE:</p>
-<p>DATA:</p></div>
-    <div class="bodyItem">
-        <p>{{ demanda.id }}</p>
-        <p>{{ demanda.produto }}</p>
-        <p>{{ demanda.quantidade }}</p>
-        <p>{{ demanda.data }}</p>
-        {{ demanda }}
-    </div>
-    </div>
-</div>
-    <button @click="modalAddIten.showModal()">Adicionar Demanda</button>
-</section>
-<dialog id="modalAddIten" ref="modalAddIten">
-        <div class="modalBody">
-        <div class="modalHeader"><h2>
-            Adicionar Demanda
-        </h2>
-        <button @click="modalAddIten.close()">X</button>
-    </div>
-        <form action="" method="post" @submit.prevent>
-        <div class="input-label">
-            <label for="">Item:</label>
-            <select name="" id="" v-model="demanda.produto">
-            <option v-for="item in estoque" :key="item.id" :value="item.id">
-            {{ item.item }}
-            </option>
-        </select>
-        </div>
-        <div class="input-label">
-            <label for="">Quantidade:</label>
-            <input type="number" placeholder="quantidade" v-model="demanda.quantidade"></div>
-            <button @click="adicionar('demandas/', demanda)">Enviar</button>
-        </form>
-    </div></dialog>
+                <div class="item" v-for="demanda in demandas" :key="demanda.id">
+                    <div class="headerItem">
+                        <p>ID:</p>
+                        <p>PRODUTO:</p>
+                        <p>QUANTIDADE:</p>
+                        <p>DATA:</p>
+                    </div>
+                    <div class="bodyItem">
+                        <p>{{ demanda.id }}</p>
+                        <p>{{ demanda.nome_produto }}</p>
+                        <p>{{ demanda.quantidade }}</p>
+                        <p>{{ demanda.data }}</p>
+                    </div>
+                </div>
+                
+            </div>
+            <button @click="modalAddIten.showModal()">Adicionar Demanda</button>
+        </section>
+        <dialog id="modalAddIten" ref="modalAddIten">
+            <div class="modalBody">
+                <div class="modalHeader">
+                    <h2>
+                        Adicionar Demanda
+                    </h2>
+                    <button @click="modalAddIten.close()">X</button>
+                </div>
+                <form action="" method="post" @submit.prevent>
+                    <div class="input-label">
+                        <label for="">Item:</label>
+                        <select name="" id="" v-model="demanda.produto">
+                            <option v-for="item in estoque" :key="item.id" :value="item.id">
+                                {{ item.item }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="input-label">
+                        <label for="">Quantidade:</label>
+                        <input type="number" placeholder="quantidade" v-model="demanda.quantidade">
+                    </div>
+                    <button @click="adicionar('demandas/', demanda)">Enviar</button>
+                </form>
+            </div>
+        </dialog>
     </main>
 </template>
 
@@ -65,7 +72,8 @@ const modalAddIten = ref(null)
 * {
     color: white;
 }
-.item{
+
+.item {
     width: 80%;
     margin-top: 2rem;
     margin-bottom: 2rem;
@@ -77,14 +85,16 @@ const modalAddIten = ref(null)
     justify-content: space-around;
     align-items: center;
 }
-.headerItem{
+
+.headerItem {
     display: flex;
     flex-direction: row;
     justify-content: space-around;
     width: 100%;
     flex-wrap: nowrap;
 }
-.headerItem > p{
+
+.headerItem>p {
     font-size: 1.2rem;
     margin-bottom: .5rem;
     margin-top: .5rem;
@@ -92,20 +102,23 @@ const modalAddIten = ref(null)
     color: #8C52FF;
     width: 20%;
 }
-.bodyItem{
+
+.bodyItem {
     display: flex;
     flex-direction: row;
     justify-content: space-around;
     width: 100%;
     flex-wrap: nowrap;
 }
-.bodyItem > p{
+
+.bodyItem>p {
     font-size: 1.2rem;
     margin-bottom: .5rem;
     margin-top: .5rem;
     text-align: start;
     width: 20%;
 }
+
 section {
     width: 70%;
     margin: 0 auto;
@@ -116,7 +129,8 @@ section {
     border-radius: .5rem;
     padding: 1rem;
 }
-section > button{
+
+section>button {
     width: 50%;
     padding: 0.5rem;
     border-radius: .5rem;
@@ -126,18 +140,21 @@ section > button{
     font-size: 16px;
     cursor: pointer;
 }
-section > button:hover{
+
+section>button:hover {
     background-color: transparent;
     color: #8C52FF;
     transition: 1s;
 }
+
 h2 {
     color: white;
     font-size: 1.8rem;
     margin-bottom: 1.5rem;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-dialog{
+
+dialog {
     width: 40%;
     margin: auto;
     /* display: flex; */
@@ -148,13 +165,15 @@ dialog{
     border: 2px solid #8C52FF;
     padding: 1rem;
 }
-.modalBody{
+
+.modalBody {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 100%;
 }
-form{
+
+form {
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -162,7 +181,8 @@ form{
     padding: 1rem;
     height: 250px;
 }
-.input-label{
+
+.input-label {
     display: flex;
     margin-top: 5%;
     margin-bottom: 5%;
@@ -170,12 +190,14 @@ form{
     flex-wrap: nowrap;
     justify-content: space-between;
 }
-.input-label > label{
+
+.input-label>label {
     font-size: 1.2rem;
     margin-bottom: .5rem;
     margin-top: .5rem;
 }
-.input-label > select{
+
+.input-label>select {
     background-color: transparent;
     color: white;
     width: 60%;
@@ -183,10 +205,11 @@ form{
     padding: 10px;
     /* text-align: center; */
     border: 2px solid #8C52FF;
-    border-radius: 1rem; 
+    border-radius: 1rem;
     /* box-shadow: 3px 3px 2px rgb(249, 255, 85); */
 }
-select > option{
+
+select>option {
     background-color: transparent;
     color: black;
     width: 60%;
@@ -194,29 +217,32 @@ select > option{
     padding: 10px;
     /* text-align: center; */
     border: 2px solid #8C52FF;
-    border-radius: 1rem; 
+    border-radius: 1rem;
     /* box-shadow: 3px 3px 2px rgb(249, 255, 85); */
 }
+
 input {
-  background-color: transparent;
-  color: white;
-  width: 60%;
-  height: 40px;
-  padding: 10px;
-  /* text-align: center; */
-  border: 2px solid #8C52FF;
-  border-radius: 1rem; 
-  /* box-shadow: 3px 3px 2px rgb(249, 255, 85); */
+    background-color: transparent;
+    color: white;
+    width: 60%;
+    height: 40px;
+    padding: 10px;
+    /* text-align: center; */
+    border: 2px solid #8C52FF;
+    border-radius: 1rem;
+    /* box-shadow: 3px 3px 2px rgb(249, 255, 85); */
 }
+
 input:focus {
-  color:  white;
-  background-color: transparent;
-  outline-color: #8C52FF;
-  box-shadow: -3px -3px 15px #8C52FF;
-  transition: .1s;
-  transition-property: box-shadow;
+    color: white;
+    background-color: transparent;
+    outline-color: #8C52FF;
+    box-shadow: -3px -3px 15px #8C52FF;
+    transition: .1s;
+    transition-property: box-shadow;
 }
-form > button{
+
+form>button {
     width: 50%;
     margin: auto;
     font-size: 16px;
@@ -228,7 +254,8 @@ form > button{
     border-radius: 1rem;
     margin-top: 1rem;
 }
-.modalHeader{
+
+.modalHeader {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -236,13 +263,15 @@ form > button{
     padding: 1rem;
     margin-bottom: 2rem;
 }
-.modalHeader > h2{
+
+.modalHeader>h2 {
     color: white;
     font-size: 1.8rem;
     margin-top: 2%;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-.modalHeader > button{
+
+.modalHeader>button {
     background-color: transparent;
     color: #8C52FF;
     width: 50px;
@@ -252,7 +281,8 @@ form > button{
     border-radius: 10px;
     cursor: pointer;
 }
-.rolagemItens{
+
+.rolagemItens {
     width: 100%;
     max-height: 450px;
     overflow-y: scroll;
