0cb7e71baaf32e875026056314e870aa279dca7b
pedrohenmalaquias <pedromalaquias2223@gmail.com>
2024-06-24 13:51:22 -0300

    fix: removendo comentários

--- src/components/ListaRanking.vue
+++ src/components/ListaRanking.vue
@@ -31,10 +31,9 @@ async function carregarColaboradores() {
         lista.value = Array.from(clienteMap.values());
         ordenado.value = lista.value
             .sort((a, b) => b.valor - a.valor)
-            .slice(0, 3); // Limita para os 3 primeiros itens após ordenar
+            .slice(0, 3);
     } catch (error) {
         console.error('Erro ao carregar colaboradores:', error);
-        // Tratar o erro, por exemplo, mostrar uma mensagem de erro ao usuário
     }
 }
 
