2487b6f892ec044d593cc4a2a9981e40501a59da
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-07-09 22:51:03 -0300

    refactor: Atualizar importações e variáveis de autenticação

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default class AuthService {
   async postUserToken(token) {
+    console.log(token)
     const response = await axios.get('http://localhost:8000/api/usuarios/me/', {
       headers: {
         Authorization: `Bearer ${token}`
